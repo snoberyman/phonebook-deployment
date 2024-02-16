ef74007fb4f6763923abe6fbd2592cb51474ce5d
Yaman Sanobar <yman.snober@gmail.com>
2024-02-16 14:31:05 -0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://server-test-en38.onrender.com',
+        target: 'https://localhost:3000',
         changeOrigin: true,
       }
     }
